dcaaa22d94d329bcac83a66a78be578a1b5083cc
ccq18 <348578429@qq.com>
2015-01-16 09:49:19 +0800

    首页及基础文件

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -2,6 +2,16 @@
 
 $pub = array(
 	//'配置项'=>'配置值'
+	'DB_TYPE'			 => 'mysql',
+	'DB_HOST'			 => 'localhost',
+	'DB_NAME'			 => 'blog',
+	'DB_USER'			 => 'root',
+	'DB_PWD'			 => '',
+	'DB_PORT'			 => '3306',
+	'DB_PREFIX'			 => 'bg_',
+	'TMPL_PARSE_STRING'  =>array(     
+	'__ASSERTS__' => __ROOT__.'/Public/Assets', // 更改默认的/Public 替换规则   
+	)
 );
 
 if(is_file(__DIR__.'/pri.php')){
